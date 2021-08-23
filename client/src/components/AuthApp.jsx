19b1d6570e752c4c20e1c481097369983b0ec788
Adrian Teng-Amnuay <pumpadrian@gmail.com>
2021-08-22 23:59:12 -0700

    Update AuthApp.jsx

--- client/src/components/AuthApp.jsx
+++ client/src/components/AuthApp.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
 import Header from './Header';
 import Home from './Home';
 
 const AuthApp = () => {
-  const { user } = useAuth();
   return (
     <Router>
       <Switch>
